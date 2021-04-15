69304b6a727a39624e0a0fd2bce3454e42fe93fc
Benjamin Navarro <navarro.benjamin13@gmail.com>
2021-04-15 12:29:51 +0200

    feat(conan): add Conan receipe

---
+++ conanfile.py
@@ -0,0 +1,50 @@
+from conans import ConanFile, CMake, tools
+
+
+class llcConan(ConanFile):
+    name = "llc"
+    version = "11.1.0"
+    license = "Apache 2.0"
+    author = "Benjamin Navarro <navarro.benjamin13@gmail.com>"
+    url = "https://github.com/BenjaminNavarro/llc"
+    description = "Collection of C++ containers extracted from LLVM"
+    topics = "C++", "LLVM", "containers"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [
+        True, False], "testing": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "testing": False}
+    generators = "cmake_find_package"
+    exports_sources = "cmake*", "include*", "source*", "test*", "CMakeLists.txt", "LICENSE.txt"
+
+    _cmake = None
+
+    def configure(self):
+        if self.settings.compiler == 'Visual Studio':
+            del self.options.fPIC
+
+    def requirements(self):
+        if self.options.testing:
+            self.requires("gtest/1.10.0")
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+        if self.options.testing:
+            cmake.test()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        else:
+            self._cmake = CMake(self)
+            self._cmake.definitions['llc_DEVELOPER_MODE'] = self.options.testing
+            self._cmake.definitions['BUILD_SHARED_LIBS'] = self.options.shared
+            self._cmake.configure(build_folder="build")
+            return self._cmake
