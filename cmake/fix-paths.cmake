28768103c93307ba4c903225bc902b8aacf695e6
Benjamin Navarro <navarro.benjamin13@gmail.com>
2021-04-15 16:45:08 +0200

    build: fix compilation with MSVC

--- cmake/fix-paths.cmake
+++ cmake/fix-paths.cmake
@@ -1,2 +1,4 @@
 # CMAKE_MODULE_PATH is passed in CI with backslashes but is difficult to fix on the CI side
-string(REPLACE "\\" "/" CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH})
+if(CMAKE_MODULE_PATH)
+    string(REPLACE "\\" "/" CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH})
+endif()
